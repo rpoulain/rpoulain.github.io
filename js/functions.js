ebfb4aa76d39965fefa63e136bbe5292cc1ab6ee
GotSave <raph48@gmail.com>
2020-04-02 01:10:37 +0200

    Smooth scroll desktop

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,3 @@
-function topFunction() {
-  $("#right-col").animate({ scrollTop: 0 }, "200");
-};
-
- $(document).ready(function() { 
-    $("#menuLink").click(function() { 
-        $("#menuDesk").slideToggle(); 
-        $("#menuOpen").fadeToggle( "slow", "linear" ); 
-        $("#menuClose").fadeToggle( "slow", "linear" ); 
-    }); 
-});
-$(document).ready(function() { 
-    $("#menuDesk").click(function() { 
-        if ($(window).width() < 1000) {
-            $("#menuDesk").slideToggle(); 
-        }
-    }); 
-});
-
 $(document).ready(function(){
     $("a").on('click', function(event) {
         if (this.hash !== "") {
@@ -24,9 +5,29 @@ $(document).ready(function(){
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function(){
+            }, 700, function(){
                 window.location.hash = hash;
             });
         }
     });
 });
+function topFunction() {
+  $('html, body').animate({ scrollTop: 0 }, 700);
+};
+ $(document).ready(function() { 
+    $("#menuLink").click(function() { 
+        $("#menuDesk").slideToggle(); 
+        $("#menuOpen").fadeToggle("slow", "linear"); 
+        $("#menuClose").fadeToggle("slow", "linear"); 
+    }); 
+});
+$(document).ready(function() { 
+    $("#menuDesk").click(function() { 
+        if ($(window).width() < 1000) {
+            $("#menuDesk").slideToggle(); 
+            $("#menuOpen").fadeToggle("slow", "linear"); 
+            $("#menuClose").fadeToggle("slow", "linear"); 
+        }
+    }); 
+});
+
